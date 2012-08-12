de01bdcc1ab87241275fecd1b3057ee2509d6043
Josef Kříž <pepakriz@gmail.com>
2012-08-12 10:03:03 +0200

    [Presenters][FrontPresenter]: Cleaned

--- Presenters/FrontPresenter.php
+++ Presenters/FrontPresenter.php
@@ -19,54 +19,5 @@ use Venne;
 class FrontPresenter extends BasePresenter
 {
 
-	const MODE_NORMAL = 0;
-
-	const MODE_MODULE = 1;
-
-	const MODE_LAYOUT = 2;
-
-	const MODE_ELEMENTS = 3;
-
-	/** @persistent */
-	public $mode = 0;
-
-
-
-	public function checkRequirements($element)
-	{
-		if ($this->mode != self::MODE_NORMAL && !$this->user->isLoggedIn()) {
-			throw new \Nette\Application\ForbiddenRequestException;
-		}
-		parent::checkRequirements($element);
-	}
-
-
-
-	public function isModeNormal()
-	{
-		return ($this->mode == self::MODE_NORMAL);
-	}
-
-
-
-	public function isModeLayout()
-	{
-		return ($this->mode == self::MODE_LAYOUT);
-	}
-
-
-
-	public function isModeModule()
-	{
-		return ($this->mode == self::MODE_MODULE);
-	}
-
-
-
-	public function isModeElements()
-	{
-		return ($this->mode == self::MODE_ELEMENTS);
-	}
-
 }
 
