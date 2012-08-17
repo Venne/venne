7fed25e19f35a90ad4c642ec7d613878dffdee4f
Josef Kříž <pepakriz@gmail.com>
2012-08-17 14:59:33 +0200

    Progress on translations.

---
+++ Resources/translations/admin.cs.php
@@ -0,0 +1,9 @@
+<?php
+return array(
+	'manage website languages, aliases,...' => 'správa jazyků a aliasů webu',
+	'add, edit users and their roles' => 'editace uživatelů a jejich rolí',
+	'manage access policies, roles,...' => 'správa přístupových oprávnění pro role',
+	'set up database, environment,...' => 'nastavení databáze, prostředí,...',
+	'manage CMS modules, donwload, install, setup,...' => 'správa CMS modulů, installace, nastavení,...',
+	'show logs, errors, warnings,...' => 'zobrazení logů, chyb a varování',
+);
