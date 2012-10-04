02f1278e3e374af5d14945d7167ebd854e82b4f3
Josef Kříž <pepakriz@gmail.com>
2012-10-04 10:46:36 +0200

    [Content][Elements]: Added EditorElement

---
+++ CmsModule/Content/Elements/EditorElement.php
@@ -0,0 +1,28 @@
+<?php
+
+/**
+ * This file is part of the Venne:CMS (https://github.com/Venne)
+ *
+ * Copyright (c) 2011, 2012 Josef Kříž (http://www.josef-kriz.cz)
+ *
+ * For the full copyright and license information, please view
+ * the file license.txt that was distributed with this source code.
+ */
+
+namespace CmsModule\Content\Elements;
+
+use Venne;
+
+/**
+ * @author Josef Kříž <pepakriz@gmail.com>
+ */
+class EditorElement extends TextElement
+{
+
+
+	public function render()
+	{
+		$this->template->text = $this->getEntity()->getText();
+		$this->template->render();
+	}
+}
