ae6254b9963617364cd4b033402a0502bcc25168
Josef Kříž <pepakriz@gmail.com>
2012-11-26 10:38:36 +0100

    Ported to latest Venne

---
+++ Module.php
@@ -0,0 +1,24 @@
+<?php
+
+/**
+ * This file is part of the Venne:CMS (https://github.com/Venne)
+ *
+ * Copyright (c) 2011, 2012 Josef Kříž (http://www.josef-kriz.cz)
+ *
+ * For the full copyright and license information, please view
+ * the file license.txt that was distributed with this source code.
+ */
+
+namespace CmsModule;
+
+use Venne;
+use Venne\Module\ComposerModule;
+
+/**
+ * @author Josef Kříž <pepakriz@gmail.com>
+ */
+class Module extends ComposerModule
+{
+
+
+}
