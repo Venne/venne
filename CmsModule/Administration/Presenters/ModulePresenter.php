84d833991f02d715c0e97e346b8f7bb142b3fcda
Josef Kříž <pepakriz@gmail.com>
2012-11-30 19:25:45 +0100

    [Administration][Presenters][ModulePresenter]: Ported to latest Venne.

--- CmsModule/Administration/Presenters/ModulePresenter.php
+++ CmsModule/Administration/Presenters/ModulePresenter.php
@@ -12,6 +12,7 @@
 namespace CmsModule\Administration\Presenters;
 
 use Venne;
+use Nette\InvalidArgumentException;
 use Venne\Module\ModuleManager;
 
 /**
@@ -23,53 +24,27 @@ class ModulePresenter extends BasePresenter
 {
 
 
-	/** @persistent */
-	public $hash;
-
 	/** @var ModuleManager */
 	protected $moduleManager;
 
-	/** @var string */
-	protected $appDir;
 
-	/** @var string */
-	protected $libsDir;
-
-	/** @var array */
-	protected $modules;
-
-
-	public function __construct(ModuleManager $moduleManager, $appDir, $libsDir, $modules)
+	/**
+	 * @param \Venne\Module\ModuleManager $moduleManager
+	 */
+	public function __construct(ModuleManager $moduleManager)
 	{
 		parent::__construct();
 
 		$this->moduleManager = $moduleManager;
-		$this->appDir = $appDir;
-		$this->libsDir = $libsDir;
-		$this->modules = $modules;
 	}
 
 
-	public function startup()
+	/**
+	 * @return \Venne\Module\ModuleManager
+	 */
+	public function getModuleManager()
 	{
-		parent::startup();
-
-		// defined as of PHP 5.4
-		if (!defined('JSON_PRETTY_PRINT')) {
-			define('JSON_PRETTY_PRINT', 128);
-		}
-
-		if (!$this->hash) {
-			$this->hash = \Nette\Utils\Strings::random();
-		}
-
-		if (!is_writable($this->libsDir)) {
-			$this->flashMessage('libsDir is not writable!', 'warning');
-		}
-
-		if (!is_writable($this->getOrigComposer()) || !is_writable($this->getOrigLock())) {
-			$this->flashMessage('composer.json and composer.lock are not writable!');
-		}
+		return $this->moduleManager;
 	}
 
 
@@ -78,15 +53,7 @@ class ModulePresenter extends BasePresenter
 	 */
 	public function actionDefault()
 	{
-		$this->template->modules = $this->moduleManager->findRepositoryModules();
-	}
-
-
-	/**
-	 * @secured(privilege="edit")
-	 */
-	public function handleEdit()
-	{
+		$this->template->modules = $this->moduleManager->findModules();
 	}
 
 
@@ -95,98 +62,80 @@ class ModulePresenter extends BasePresenter
 	 */
 	public function handleSync()
 	{
-		$this->moduleManager->scanRepositoryModules();
-		$this->flashMessage('Database has been synced.', 'success');
+		$this->moduleManager->update();
+		$this->flashMessage('List has been synced.', 'success');
 		$this->redirect('this');
 	}
 
 
-	protected function createComponentDownloadForm()
+	/**
+	 * @secured(privilege="edit")
+	 */
+	public function handleInstall($name, $confirm = false)
 	{
-		$require = $this->modules;
+		$module = $this->moduleManager->createInstance($name);
 
-		$form = new \Venne\Forms\Form();
-		$form->getElementPrototype()->attrs['class'] = 'noAjax';
-		$form->addSubmit('_submit', 'Sync');
-		$form->onValidate[] = function($form)
-		{
-			$form->getPresenter()->tryCall('handleEdit', array());
-		};
-		$form->onSuccess[] = $this->save;
-
-		foreach ($this->moduleManager->findRepositoryModules() as $name => $items) {
-			$versions = explode(', ', $items->versions);
-
-			if (($pos = array_search('* dev-master', $versions)) !== false || ($pos = array_search('dev-master', $versions)) !== false) {
-				unset($versions[$pos]);
-			}
-
-			$form->addCheckbox('check_' . $name);
-			$form->addSelect('version_' . $name)->setItems($versions, false);
-
-			if (isset($require[$name])) {
-				$form['check_' . $name]->setDefaultValue(true);
-				$form['version_' . $name]->setDefaultValue($require[$name]['version']);
-			}
+		try {
+			$problem = $this->moduleManager->testInstall($module);
+		} catch (InvalidArgumentException $e) {
+			$this->flashMessage($e->getMessage(), 'warning');
+			$this->redirect('this');
 		}
 
-		return $form;
-	}
-
-
-	public function save(\Venne\Forms\Form $form)
-	{
-		$values = $form->getValues();
-		$data = json_decode(file_get_contents($this->getOrigComposer()), true);
-
-		foreach ($this->moduleManager->findRepositoryModules() as $name => $items) {
-			if ($values["check_$name"]) {
-				$data['require']["venne/{$name}-module"] = $values["version_$name"];
-			} else {
-				if (isset($data['require']["venne/{$name}-module"])) {
-					unset($data['require']["venne/{$name}-module"]);
-				}
-			}
+		if (!$confirm && count($problem->getSolutions()) > 0) {
+			$this->template->solutions = $problem->getSolutions();
+			$this->template->solutionAction = 'uninstall';
+			$this->template->solutionModule = $name;
+			return;
 		}
 
-		file_put_contents($this->getComposer(), str_replace('\/', '/', json_encode($data, JSON_PRETTY_PRINT)));
-		copy($this->getOrigLock(), $this->getLock());
-
-		$ret = $this->moduleManager->updateByComposer($this->getComposer());
-
-		unlink($this->getOrigComposer());
-		unlink($this->getOrigLock());
-		copy($this->getComposer(), $this->getOrigComposer());
-		copy($this->getLock(), $this->getOrigLock());
-
-		$el = \Nette\Utils\Html::el('pre');
-		$el->setHtml($ret);
-		$this->flashMessage($el, strpos($ret, 'Generating autoload files') !== false ? 'success' : 'warning');
+		try {
+			foreach ($problem->getSolutions() as $job) {
+				$this->moduleManager->doAction($job->getAction(), $job->getModule());
+				$this->flashMessage("Module '{$job->getModule()->getName()}' has been installed.", 'success');
+			}
+			$this->moduleManager->install($module);
+			$this->flashMessage("Module '{$name}' has been installed.", 'success');
+		} catch (InvalidArgumentException $e) {
+			$this->flashMessage($e->getMessage(), 'warning');
+		}
 
 		$this->redirect('this');
 	}
 
 
-	protected function getOrigComposer()
+	/**
+	 * @secured(privilege="edit")
+	 */
+	public function handleUninstall($name, $confirm = false)
 	{
-		return $this->appDir . '/../composer.json';
-	}
+		$module = $this->moduleManager->createInstance($name);
 
+		try {
+			$problem = $this->moduleManager->testUninstall($module);
+		} catch (InvalidArgumentException $e) {
+			$this->flashMessage($e->getMessage(), 'warning');
+			$this->redirect('this');
+		}
 
-	protected function getOrigLock()
-	{
-		return $this->appDir . '/../composer.lock';
-	}
+		if (!$confirm && count($problem->getSolutions()) > 0) {
+			$this->template->solutions = $problem->getSolutions();
+			$this->template->solutionAction = 'uninstall';
+			$this->template->solutionModule = $name;
+			return;
+		}
 
+		try {
+			foreach ($problem->getSolutions() as $job) {
+				$this->moduleManager->doAction($job->getAction(), $job->getModule());
+				$this->flashMessage("Module '{$job->getModule()->getName()}' has been uninstalled.", 'success');
+			}
+			$this->moduleManager->uninstall($module);
+			$this->flashMessage("Module '{$name}' has been uninstalled.", 'success');
+		} catch (InvalidArgumentException $e) {
+			$this->flashMessage($e->getMessage(), 'warning');
+		}
 
-	protected function getComposer()
-	{
-		return $this->appDir . "/composer-{$this->hash}.json";
-	}
-
-
-	protected function getLock()
-	{
-		return $this->appDir . "/composer-{$this->hash}.lock";
+		$this->redirect('this');
 	}
 }
