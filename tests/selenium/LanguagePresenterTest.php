2fb64b7820bbbcd23d989d52a131e4bd8f646d7a
Josef Kříž <pepakriz@gmail.com>
2013-01-16 11:27:12 +0100

    [tests][LanguagePresenterTest]: Fixed selector for new table component.

--- tests/selenium/LanguagePresenterTest.php
+++ tests/selenium/LanguagePresenterTest.php
@@ -25,46 +25,26 @@ class LanguagePresenterTest extends AdminPresenterTest
 
 		$this->clickAndWait("link=Language settingsManage website languages, aliases,...");
 		$this->clickAndWait("link=Create new");
-  		$this->type("id=frmcreateForm-name", "test1");
+		$this->type("id=frmcreateForm-name", "test1");
 		$this->type("id=frmcreateForm-short", "test2");
 		$this->type("id=frmcreateForm-alias", "test3");
 		$this->clickAndWait("id=frmcreateForm-_submit");
-		try {
-			$this->assertEquals("test1", $this->getTable("id=snippet-table-table.2.0"));
-		} catch (PHPUnit_Framework_AssertionFailedError $e) {
-			array_push($this->verificationErrors, $e->toString());
-		}
-		try {
-			$this->assertEquals("test3", $this->getTable("id=snippet-table-table.2.1"));
-		} catch (PHPUnit_Framework_AssertionFailedError $e) {
-			array_push($this->verificationErrors, $e->toString());
-		}
-		try {
-			$this->assertEquals("test2", $this->getTable("id=snippet-table-table.2.2"));
-		} catch (PHPUnit_Framework_AssertionFailedError $e) {
-			array_push($this->verificationErrors, $e->toString());
-		}
-		$this->clickAndWait("xpath=(//a[contains(text(),'Edit')])[3]");
+
+		$this->assertEquals("test1", $this->getTable("id=snippet-table-table.2.1"));
+		$this->assertEquals("test3", $this->getTable("id=snippet-table-table.2.2"));
+		$this->assertEquals("test2", $this->getTable("id=snippet-table-table.2.3"));
+
+		$this->clickAndWait("xpath=(//a[contains(text(),'Edit')])[4]");
 		$this->type("id=frmeditForm-name", "test4");
 		$this->type("id=frmeditForm-short", "test5");
 		$this->type("id=frmeditForm-alias", "test6");
 		$this->clickAndWait("id=frmeditForm-_submit");
-		try {
-			$this->assertEquals("test4", $this->getTable("id=snippet-table-table.2.0"));
-		} catch (PHPUnit_Framework_AssertionFailedError $e) {
-			array_push($this->verificationErrors, $e->toString());
-		}
-		try {
-			$this->assertEquals("test6", $this->getTable("id=snippet-table-table.2.1"));
-		} catch (PHPUnit_Framework_AssertionFailedError $e) {
-			array_push($this->verificationErrors, $e->toString());
-		}
-		try {
-			$this->assertEquals("test5", $this->getTable("id=snippet-table-table.2.2"));
-		} catch (PHPUnit_Framework_AssertionFailedError $e) {
-			array_push($this->verificationErrors, $e->toString());
-		}
-		$this->click("xpath=(//a[contains(text(),'Delete')])[3]");
+
+		$this->assertEquals("test4", $this->getTable("id=snippet-table-table.2.1"));
+		$this->assertEquals("test6", $this->getTable("id=snippet-table-table.2.2"));
+		$this->assertEquals("test5", $this->getTable("id=snippet-table-table.2.3"));
+
+		$this->click("xpath=(//a[contains(text(),'Delete')])[4]");
 
 		$this->logout();
 	}
