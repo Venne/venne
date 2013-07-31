8246345ea44675dfe3a2ecfcac2790e4ba131668
Josef Kříž <pepakriz@gmail.com>
2013-07-31 09:58:41 +0200

    Better instalation proccess.

--- CmsModule/Administration/Presenters/InstallationPresenter.php
+++ CmsModule/Administration/Presenters/InstallationPresenter.php
@@ -11,14 +11,18 @@
 
 namespace CmsModule\Administration\Presenters;
 
+use CmsModule\Content\Entities\LanguageEntity;
 use CmsModule\Forms\LanguageFormFactory;
 use CmsModule\Forms\SystemAccountFormFactory;
 use CmsModule\Forms\SystemDatabaseFormFactory;
 use CmsModule\Pages\Text\PageEntity;
+use Doctrine\ORM\Tools\SchemaTool;
 use Nette\Caching\Cache;
 use Nette\Caching\IStorage;
 use Nette\Caching\Storages\MemoryStorage;
+use Nette\InvalidArgumentException;
 use Nette\Loaders\RobotLoader;
+use Nette\Reflection\ClassType;
 use Venne\Security\Authenticator;
 
 /**
@@ -124,15 +128,15 @@ class InstallationPresenter extends BasePresenter
 		}
 
 		// Resources dir
-		if (!file_exists($this->resourcesDir . "/cmsModule")) {
-			@symlink("../../vendor/venne/cms-module/Resources/public", $this->resourcesDir . "/cmsModule");
+		if (!file_exists($this->resourcesDir . '/cmsModule')) {
+			@symlink("../../vendor/venne/cms-module/Resources/public", $this->resourcesDir . '/cmsModule');
 		}
 
 		// Extensions
 		$modules = array('iconv', 'json', "pdo");
 		foreach ($modules as $item) {
 			if (!extension_loaded($item)) {
-				$this->flashMessage("Module " . $item . " is not enabled.", "warning");
+				$this->flashMessage("Module {$item} is not enabled.", 'warning');
 			}
 		}
 
@@ -140,7 +144,7 @@ class InstallationPresenter extends BasePresenter
 		$paths = array($this->resourcesDir, $this->dataDir, $this->configDir, $this->configDir . '/config.neon', $this->tempDir, $this->wwwCacheDir, $this->publicDir);
 		foreach ($paths as $item) {
 			if (!is_writable($item)) {
-				$this->flashMessage("Path " . $item . " is not writable.", "warning");
+				$this->flashMessage("Path {$item} is not writable.", 'warning');
 			}
 		}
 	}
@@ -171,45 +175,69 @@ class InstallationPresenter extends BasePresenter
 			/** @var $installer CmsInstaller */
 			$installer = $this->context->createInstance('CmsModule\Module\Installers\CmsInstaller');
 			$installer->install($this->context->venne->moduleManager->modules['cms']);
-
-
-			// pages
-			$textPage = new PageEntity;
-			$textPage
-				->getExtendedMainRoute()
-				->setName('Main page')
-				->getRoute()
-				->setText('Hello, this is main page of this website.')
-				->setPublished(TRUE);
-
-			$userPage = new \CmsModule\Pages\Users\PageEntity;
-			$userPage
-				->getPage()
-				->setParent($textPage);
-			$userPage
-				->getExtendedMainRoute()
-				->setName('Users')
-				->getRoute()
-				->setText('List of users.')
-				->setPublished(TRUE);
-
-			$tagsPage = new \CmsModule\Pages\Tags\PageEntity;
-			$tagsPage
-				->getPage()
-				->setParent($textPage);
-			$tagsPage
-				->getExtendedMainRoute()
-				->setName('Tags')
-				->getRoute()
-				->setText('List of tags.')
-				->setPublished(TRUE);
-
-			$em->persist($textPage);
-			$em->persist($userPage);
-			$em->persist($tagsPage);
-			$em->flush();
 		}
-		$this->redirect("Intsallation:");
+
+		$this->redirect('Installation:');
+	}
+
+
+	public function handleCreateStructure()
+	{
+		$em = $this->context->entityManager;
+
+		if (!$this->isEmptyStructure()) {
+			throw new InvalidArgumentException("Page structure must be empty");
+		}
+
+		$layout = $em->getRepository('CmsModule\Content\Entities\LayoutEntity')->find(1);
+
+		// pages
+		$textPage = new PageEntity;
+		$textPage
+			->getExtendedMainRoute()
+			->setName('Main page')
+			->getRoute()
+			->setLayout($layout)
+			->setText('Hello, this is main page of this website.')
+			->setPublished(TRUE);
+
+		$userPage = new \CmsModule\Pages\Users\PageEntity;
+		$userPage
+			->getPage()
+			->setParent($textPage->getPage());
+		$userPage
+			->getExtendedMainRoute()
+			->setName('Users')
+			->getRoute()
+			->setText('List of users.')
+			->setPublished(TRUE);
+
+		$tagsPage = new \CmsModule\Pages\Tags\PageEntity;
+		$tagsPage
+			->getPage()
+			->setParent($textPage->getPage());
+		$tagsPage
+			->getExtendedMainRoute()
+			->setName('Tags')
+			->getRoute()
+			->setText('List of tags.')
+			->setPublished(TRUE);
+
+		$em->persist($textPage);
+		$em->persist($userPage);
+		$em->persist($tagsPage);
+		$em->flush();
+
+		$this->redirect('Dashboard:');
+	}
+
+
+	/**
+	 * @return bool
+	 */
+	public function isEmptyStructure()
+	{
+		return !$this->context->entityManager->getRepository('CmsModule\Content\Entities\RouteEntity')->createQueryBuilder('a')->select('COUNT(a.id)')->getQuery()->getSingleScalarResult();
 	}
 
 
@@ -224,14 +252,14 @@ class InstallationPresenter extends BasePresenter
 	protected function createComponentDatabaseForm()
 	{
 		$form = $this->databaseFormFactory->invoke();
-		$form->onSuccess[] = $this->databaseFormSuccess();
+		$form->onSuccess[] = $this->databaseFormSuccess;
 		return $form;
 	}
 
 
 	protected function createComponentLanguageForm()
 	{
-		$form = $this->languageFormFactory->invoke();
+		$form = $this->languageFormFactory->invoke(new LanguageEntity);
 		$form->onSuccess[] = $this->languageFormSuccess;
 		return $form;
 	}
@@ -249,18 +277,18 @@ class InstallationPresenter extends BasePresenter
 		$cache = new Cache($this->cacheStorage, 'Nette.Configurator');
 		$cache->clean();
 
-		$this->redirect("Installation:");
+		$this->redirect('Installation:');
 	}
 
 
 	public function databaseFormSuccess()
 	{
-		$this->redirect("Installation:install!");
+		$this->redirect('install!');
 	}
 
 
 	public function languageFormSuccess()
 	{
-		$this->redirect("Installation:");
+		$this->redirect('Installation:');
 	}
 }
