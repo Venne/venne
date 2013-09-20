23ecc2215c5a7725576cc3efdf76d18ef3ed0e14
Josef Kříž <pepakriz@gmail.com>
2013-09-20 12:56:22 +0200

    admin\application.js: Compatibility with history extension.

--- Resources/public/admin/js/application.js
+++ Resources/public/admin/js/application.js
@@ -71,31 +71,22 @@ $(function () {
 		}
 	});
 	$.nette.ext('tooltipBind', {
-		init: function () {
+		load: function () {
 			this.init($('body'));
-		},
-		success: function (payload) {
-			if (!payload.snippets) {
-				return;
-			}
-
-			var _this = this;
-			for (var i in payload.snippets) {
-				$('#' + i).each(function () {
-					_this.init($(this));
-				});
-			}
 		}
 	}, {
 		init: function (target) {
 			target.find(this.selector).each(function () {
-				$(this).tooltip();
+				if (!$(this).data('ajaxTooltipCreated')) {
+					$(this).data('ajaxTooltipCreated', true);
+					$(this).tooltip();
+				}
 			});
 		},
 		selector: "[data-toggle='tooltip']"
 	});
 	$.nette.ext('formsMultiSelectBind', {
-		init: function () {
+		load: function () {
 			this.init($('body'));
 		},
 		success: function (payload) {
@@ -113,89 +104,84 @@ $(function () {
 	}, {
 		init: function (target) {
 			target.find(this.selector).each(function () {
-				var args = {
-					width: 'resolve'
-				};
+				if (!$(this).data('formsMultiSelectBind')) {
+					$(this).data('formsMultiSelectBind', true);
+					var args = {
+						width: 'resolve'
+					};
 
-				if ($(this).attr('data-tag-suggest') !== undefined) {
-					args.createSearchChoice = function (term, data) {
-						if ($(data).filter(function () {
-							return this.text.localeCompare(term) === 0;
-						}).length === 0) {
-							return {
-								id: term,
-								text: term
-							};
-						}
-					};
-					args.initSelection = function (element, callback) {
-						var data = [];
-						$(element.val().split($(element).attr('data-tag-joiner'))).each(function () {
-							data.push({id: this, text: this});
-						});
-						callback(data);
-					};
-					args.ajax = {
-						url: $(this).attr('data-tag-suggest'),
-						dataType: 'json',
-						results: function (data) {
-							var results = [];
-							$.each(data.results, function (index, item) {
-								results.push({
-									id: index,
-									text: item
-								});
-							});
-							return {
-								results: results
-							};
-						},
-						data: function (term, page) {
-							return {
-								q: term
+					if ($(this).attr('data-tag-suggest') !== undefined) {
+						args.createSearchChoice = function (term, data) {
+							if ($(data).filter(function () {
+								return this.text.localeCompare(term) === 0;
+							}).length === 0) {
+								return {
+									id: term,
+									text: term
+								};
 							}
-						}
-					};
-				}
+						};
+						args.initSelection = function (element, callback) {
+							var data = [];
+							$(element.val().split($(element).attr('data-tag-joiner'))).each(function () {
+								data.push({id: this, text: this});
+							});
+							callback(data);
+						};
+						args.ajax = {
+							url: $(this).attr('data-tag-suggest'),
+							dataType: 'json',
+							results: function (data) {
+								var results = [];
+								$.each(data.results, function (index, item) {
+									results.push({
+										id: index,
+										text: item
+									});
+								});
+								return {
+									results: results
+								};
+							},
+							data: function (term, page) {
+								return {
+									q: term
+								}
+							}
+						};
+					}
 
-				if ($(this).attr('data-tag-joiner') !== undefined) {
-					var tags = jQuery.parseJSON($(this).attr('data-tags'));
-					args.tags = true;
-					//args.tokenSeparators = [$(this).attr('data-tag-joiner')];
-					args.separator = $(this).attr('data-tag-joiner');
-				}
+					if ($(this).attr('data-tag-joiner') !== undefined) {
+						var tags = jQuery.parseJSON($(this).attr('data-tags'));
+						args.tags = true;
+						//args.tokenSeparators = [$(this).attr('data-tag-joiner')];
+						args.separator = $(this).attr('data-tag-joiner');
+					}
 
-				$(this).select2(args);
+					$(this).select2(args);
+				}
 			});
 		},
 		selector: 'select[multiple], input[data-tag-joiner]'
 	});
 	$.nette.ext('formsDateInputBind', {
-		init: function () {
+		load: function () {
 			this.init($('body'));
-		},
-		success: function (payload) {
-			if (!payload.snippets) {
-				return;
-			}
-
-			var _this = this;
-			for (var i in payload.snippets) {
-				$('#' + i).each(function () {
-					_this.init($(this));
-				});
-			}
 		}
 	}, {
 		init: function (target) {
 			target.find(this.selector).each(function () {
-				var e = $(this);
-				e.wrap('<div class="input-group date" id="datetimepicker-' + $(this).attr('id') + '" />')
-					.after('<span class="input-group-addon"><i class="glyphicon glyphicon-calendar"></i></span>')
-					.parent('div');
-				$('#datetimepicker-' + $(this).attr('id')).datetimepicker({
-					format: 'yyyy-MM-dd hh:mm:ss'
-				});
+				if (!$(this).data('formsDateInputBind')) {
+					$(this).data('formsDateInputBind', true);
+
+					var e = $(this);
+					e.wrap('<div class="input-group date" id="datetimepicker-' + $(this).attr('id') + '" />')
+						.after('<span class="input-group-addon"><i class="glyphicon glyphicon-calendar"></i></span>')
+						.parent('div');
+					$('#datetimepicker-' + $(this).attr('id')).datetimepicker({
+						format: 'yyyy-MM-dd hh:mm:ss'
+					});
+				}
 			});
 		},
 		selector: 'input[type=date], input[type=datetime]'
@@ -208,48 +194,36 @@ $(function () {
 		}
 	});
 	$.nette.ext('formsIframePostBind', {
-		init: function () {
-			this.init(this.selector);
-		},
-		success: function (payload) {
-			if (!payload.snippets) {
-				return;
-			}
-
-			for (var i in payload.snippets) {
-				this.init('#' + i + ' ' + this.selector);
-			}
+		load: function () {
+			this.init($('body'));
 		}
 	}, {
 		init: function (target) {
-			$(target).parents('form').each(function () {
-				$(this).removeClass('ajax');
-				var _id = $(this).attr('id');
+			target.find(this.selector).each(function () {
+				$(this).parents('form').each(function () {
+					if (!$(this).data('formsIframePostBind')) {
+						$(this).data('formsIframePostBind', true);
 
-				$(this).iframePostForm({
-					iframeID: this.idPrefix + _id,
-					complete: function (response) {
-						url = $('#' + this.idPrefix + _id).get(0).contentWindow.location;
-						$.nette.ajax({url: url});
+						$(this).removeClass('ajax');
+						var _id = $(this).attr('id');
+
+						$(this).iframePostForm({
+							iframeID: this.idPrefix + _id,
+							complete: function (response) {
+								url = $('#' + this.idPrefix + _id).get(0).contentWindow.location;
+								$.nette.ajax({url: url});
+							}
+						});
 					}
-				})
+				});
 			});
 		},
 		selector: 'form.ajax input:file',
 		idPrefix: 'iframe-post-form-'
 	});
 	$.nette.ext('formsFileUpload', {
-		init: function () {
+		load: function () {
 			this.init('body');
-		},
-		success: function (payload) {
-			if (!payload.snippets) {
-				return;
-			}
-
-			for (var i in payload.snippets) {
-				this.init('#' + i);
-			}
 		}
 	}, {
 		init: function (target) {
@@ -269,28 +243,32 @@ $(function () {
 				});
 			}
 			$(target).find('input[type="file"]').each(function () {
-				var fileInput = $(this);
-				$(this).after('<div class="input-group">'
-					+ '<div class="form-control input-sm" id="' + $(this).attr('id') + '_fake" type="text" disabled></div>'
-					+ '<div class="input-group-btn btn-group">'
-					+ '<button class="btn btn-default btn-sm hide" id="' + $(this).attr('id') + '_fakeRemove" type="button">Remove</button>'
-					+ '<button class="btn btn-default btn-sm" id="' + $(this).attr('id') + '_fakeButton" type="button">Select file</button>'
-					+ '</div>'
-					+ '</div>');
-				$('#' + $(this).attr('id') + '_fakeButton').off('click');
-				$('#' + $(this).attr('id') + '_fakeRemove').off('click');
-				$('#' + $(this).attr('id') + '_fakeButton').on('click', function () {
-					fileInput.click();
-				});
-				$('#' + $(this).attr('id') + '_fakeRemove').on('click', function () {
-					fileInput.replaceWith(fileInput.clone());
-					$('#' + fileInput.attr('id') + '_fake').html('');
-					$('#' + fileInput.attr('id') + '_fakeRemove').hide();
-					$('#' + fileInput.attr('id') + '_fakeButton').text('Select file');
-					changeFunc(fileInput);
-				});
-				changeFunc($(this));
-				$(this).hide();
+				if (!$(this).data('formsFileUpload')) {
+					$(this).data('formsFileUpload', true);
+
+					var fileInput = $(this);
+					$(this).after('<div class="input-group">'
+						+ '<div class="form-control input-sm" id="' + $(this).attr('id') + '_fake" type="text" disabled></div>'
+						+ '<div class="input-group-btn btn-group">'
+						+ '<button class="btn btn-default btn-sm hide" id="' + $(this).attr('id') + '_fakeRemove" type="button">Remove</button>'
+						+ '<button class="btn btn-default btn-sm" id="' + $(this).attr('id') + '_fakeButton" type="button">Select file</button>'
+						+ '</div>'
+						+ '</div>');
+					$('#' + $(this).attr('id') + '_fakeButton').off('click');
+					$('#' + $(this).attr('id') + '_fakeRemove').off('click');
+					$('#' + $(this).attr('id') + '_fakeButton').on('click', function () {
+						fileInput.click();
+					});
+					$('#' + $(this).attr('id') + '_fakeRemove').on('click', function () {
+						fileInput.replaceWith(fileInput.clone());
+						$('#' + fileInput.attr('id') + '_fake').html('');
+						$('#' + fileInput.attr('id') + '_fakeRemove').hide();
+						$('#' + fileInput.attr('id') + '_fakeButton').text('Select file');
+						changeFunc(fileInput);
+					});
+					changeFunc($(this));
+					$(this).hide();
+				}
 			});
 		}
 	});
@@ -310,23 +288,14 @@ $(function () {
 	$.nette.ext('textWithSelectBind', {
 		load: function () {
 			this.init($('body'));
-		},
-		success: function (payload) {
-			if (!payload.snippets) {
-				return;
-			}
-
-			var _this = this;
-			for (var i in payload.snippets) {
-				$('#' + i).each(function () {
-					_this.init($(this));
-				});
-			}
 		}
 	}, {
 		init: function (target) {
 			target.find(this.selector).each(function () {
-				$(this).textWithSelect();
+				if (!$(this).data('textWithSelectBind')) {
+					$(this).data('textWithSelectBind', true);
+					$(this).textWithSelect();
+				}
 			});
 		},
 		selector: "form .input-group-btn"
