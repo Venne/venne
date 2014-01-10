d00af8215a4ea78924ba56534a9ccb02790b4985
Josef Kříž <pepakriz@gmail.com>
2014-01-10 14:19:06 +0100

    New page type: userlist

---
+++ CmsModule/Pages/Userlist/PageEntity.php
@@ -0,0 +1,77 @@
+<?php
+
+/**
+ * This file is part of the Venne:CMS (https://github.com/Venne)
+ *
+ * Copyright (c) 2011, 2012 Josef Kříž (http://www.josef-kriz.cz)
+ *
+ * For the full copyright and license information, please view
+ * the file license.txt that was distributed with this source code.
+ */
+
+namespace CmsModule\Pages\Userlist;
+
+use CmsModule\Content\Entities\ExtendedPageEntity;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @author Josef Kříž <pepakriz@gmail.com>
+ * @ORM\Entity(repositoryClass="\CmsModule\Content\Repositories\PageRepository")
+ * @ORM\Table(name="userlist_page")
+ */
+class PageEntity extends ExtendedPageEntity
+{
+
+	/**
+	 * @var string
+	 * @ORM\Column(type="integer")
+	 */
+	protected $itemsPerPage = 10;
+
+	/**
+	 * @var \Doctrine\Common\Collections\ArrayCollection|\CmsModule\Security\Entities\RoleEntity[]
+	 * @ORM\ManyToMany(targetEntity="\CmsModule\Security\Entities\RoleEntity")
+	 * @ORM\JoinTable(name="userlist_page_roles",
+	 *      joinColumns={@ORM\JoinColumn(name="page_id", referencedColumnName="id", onDelete="CASCADE")},
+	 *      inverseJoinColumns={@ORM\JoinColumn(name="role_id", referencedColumnName="id", onDelete="CASCADE")}
+	 *      )
+	 */
+	protected $roles;
+
+
+	/**
+	 * @param string $itemsPerPage
+	 */
+	public function setItemsPerPage($itemsPerPage)
+	{
+		$this->itemsPerPage = $itemsPerPage;
+	}
+
+
+	/**
+	 * @return string
+	 */
+	public function getItemsPerPage()
+	{
+		return $this->itemsPerPage;
+	}
+
+
+	/**
+	 * @param \CmsModule\Security\Entities\RoleEntity[]|\Doctrine\Common\Collections\ArrayCollection $roles
+	 */
+	public function setRoles($roles)
+	{
+		$this->roles = $roles;
+	}
+
+
+	/**
+	 * @return \CmsModule\Security\Entities\RoleEntity[]|\Doctrine\Common\Collections\ArrayCollection
+	 */
+	public function getRoles()
+	{
+		return $this->roles;
+	}
+
+}
---
+++ CmsModule/Pages/Userlist/PageFormFactory.php
@@ -0,0 +1,35 @@
+<?php
+
+/**
+ * This file is part of the Venne:CMS (https://github.com/Venne)
+ *
+ * Copyright (c) 2011, 2012 Josef Kříž (http://www.josef-kriz.cz)
+ *
+ * For the full copyright and license information, please view
+ * the file license.txt that was distributed with this source code.
+ */
+
+namespace CmsModule\Pages\Userlist;
+
+use DoctrineModule\Forms\FormFactory;
+use Venne\Forms\Form;
+
+/**
+ * @author Josef Kříž <pepakriz@gmail.com>
+ */
+class PageFormFactory extends FormFactory
+{
+
+	/**
+	 * @param Form $form
+	 */
+	public function configure(Form $form)
+	{
+		$form->addGroup('Settings');
+		$form->addText('itemsPerPage', 'Items per page');
+		$form->addManyToMany('roles', 'Roles');
+
+		$form->addGroup();
+		$form->addSaveButton('Save');
+	}
+}
