736679c0a142ebbf39269e23364fb99d8935d474
Josef Kříž <pepakriz@gmail.com>
2014-09-19 18:57:18 +0200

    Ported to latest Grido

--- src/Bridges/Grido/Grid.php
+++ src/Bridges/Grido/Grid.php
@@ -11,7 +11,7 @@
 
 namespace Venne\Bridges\Grido;
 
-use Venne\Bridges\Grido\PropertyAccessors\SymfonyPropertyAccessor;
+use Grido\PropertyAccessors\SymfonyPropertyAccessor;
 
 /**
  * @author Josef Kříž <pepakriz@gmail.com>
@@ -20,8 +20,7 @@ class Grid extends \Grido\Grid
 {
 
 	/**
-	 * @return \Venne\Bridges\Grido\PropertyAccessors\SymfonyPropertyAccessor
-	 * @internal
+	 * @return \Grido\PropertyAccessors\SymfonyPropertyAccessor
 	 */
 	public function getPropertyAccessor()
 	{
@@ -30,79 +29,6 @@ class Grid extends \Grido\Grid
 		}
 
 		return $this->propertyAccessor;
-	}
-
-	/**
-	 * @param string $name
-	 * @param boolean $need
-	 * @return \Grido\Components\Columns\Editable
-	 */
-	public function getColumn($name, $need = true)
-	{
-		return parent::getColumn($this->formatName($name), $need);
-	}
-
-	/**
-	 * @param string $name
-	 * @param string $label
-	 * @return \Grido\Components\Columns\Text
-	 */
-	public function addColumnText($name, $label)
-	{
-		return parent::addColumnText($this->formatName($name), $label);
-	}
-
-	/**
-	 * @param string $name
-	 * @param string $label
-	 * @return \Grido\Components\Columns\Email
-	 */
-	public function addColumnEmail($name, $label)
-	{
-		return parent::addColumnEmail($this->formatName($name), $label);
-	}
-
-	/**
-	 * @param string $name
-	 * @param string $label
-	 * @return \Grido\Components\Columns\Href
-	 */
-	public function addColumnHref($name, $label)
-	{
-		return parent::addColumnHref($this->formatName($name), $label);
-	}
-
-	/**
-	 * @param string $name
-	 * @param string $label
-	 * @param string $dateFormat
-	 * @return \Grido\Components\Columns\Date
-	 */
-	public function addColumnDate($name, $label, $dateFormat = null)
-	{
-		return parent::addColumnDate($this->formatName($name), $label, $dateFormat);
-	}
-
-	/**
-	 * @param string $name
-	 * @param string $label
-	 * @param int $decimals number of decimal points
-	 * @param string $decPoint separator for the decimal point
-	 * @param string $thousandsSep thousands separator
-	 * @return \Grido\Components\Columns\Number
-	 */
-	public function addColumnNumber($name, $label, $decimals = null, $decPoint = null, $thousandsSep = null)
-	{
-		return parent::addColumnNumber($this->formatName($name), $label, $decimals, $decPoint, $thousandsSep);
-	}
-
-	/**
-	 * @param string $name
-	 * @return string
-	 */
-	private function formatName($name)
-	{
-		return str_replace('.', '__', $name);
 	}
 
 }
